76e8694151f7e587dab51a387b3a9175dab81f97
wpalchemist <morgan@wpalchemists.com>
2019-02-12 17:24:57 -0800

    phpbf fixes

--- admin/class-book-sync-cli.php
+++ admin/class-book-sync-cli.php
@@ -61,13 +61,13 @@ class Book_Sync_Cli extends WP_CLI_Command {
 	 * Initialize the class and set its properties.
 	 *
 	 * @since      1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
+	 * @param      string $plugin_name       The name of this plugin.
+	 * @param      string $version    The version of this plugin.
 	 */
-	public function __construct( ) {
+	public function __construct() {
 
 		$book_sync_options = new Book_Sync_Options( $this->plugin_name, $this->version );
-		$this->options = $book_sync_options->get_options();
+		$this->options     = $book_sync_options->get_options();
 
 	}
 
@@ -87,10 +87,10 @@ class Book_Sync_Cli extends WP_CLI_Command {
 	 * @param $assoc_args array
 	 */
 	public function import_books( $args, $assoc_args ) {
-		if( 'LibraryThing' === $assoc_args['source'] ) {
+		if ( 'LibraryThing' === $assoc_args['source'] ) {
 			WP_CLI::line( 'Importing books from LibraryThing' );
 			$book_list = $this->retrieve_books_from_librarything();
-		} elseif( 'Goodreads' === $assoc_args['source'] ) {
+		} elseif ( 'Goodreads' === $assoc_args['source'] ) {
 			WP_CLI::line( 'Importing books from Goodreads' );
 			$book_list = 'tbd';
 		} else {
@@ -98,7 +98,7 @@ class Book_Sync_Cli extends WP_CLI_Command {
 			return;
 		}
 
-		foreach( $book_list as $book ) {
+		foreach ( $book_list as $book ) {
 			$this->add_book( $book );
 		}
 	}
@@ -113,36 +113,36 @@ class Book_Sync_Cli extends WP_CLI_Command {
 		$books = array();
 
 		$userid = $this->options['librarything_username'];
-		if( '' === $userid ) {
+		if ( '' === $userid ) {
 			WP_CLI::error( 'You must go to the plugin options page and enter your LibraryThing username' );
 			die;
 		}
 
 		$key = $this->options['librarything_username'];
-		if( '' === $key ) {
+		if ( '' === $key ) {
 			WP_CLI::error( 'You must go to the plugin options page and enter your LibraryThing key' );
 			die;
 		}
 
 		// @todo - change "max" to a really high number, or maybe make it a parameter
 		$raw_books = wp_remote_get( 'https://www.librarything.com/api_getdata.php?userid=' . $userid . '&key=' . $key . '&limit=review&max=5&reviewmax=10000&booksort=title&responseType=json&resultsets=books,bookratings&showReviews=1' );
-		if( is_wp_error( $raw_books ) ) {
+		if ( is_wp_error( $raw_books ) ) {
 			WP_CLI::error( $raw_books );
 			die;
 		}
 
-		$body = json_decode( $raw_books['body']);
-		foreach( $body->books as $book ) {
+		$body = json_decode( $raw_books['body'] );
+		foreach ( $body->books as $book ) {
 			WP_CLI::debug( print_r( $book, true ) );
-			$books[$book->book_id] = array(
-				'lt_id' => $book->book_id,
-				'title' => $book->title,
-				'author' => $book->author_lf,
-				'ISBN' => $book->ISBN,
+			$books[ $book->book_id ] = array(
+				'lt_id'       => $book->book_id,
+				'title'       => $book->title,
+				'author'      => $book->author_lf,
+				'ISBN'        => $book->ISBN,
 				'publication' => $book->publicationdate,
-				'rating' => $book->rating,
-				'cover' => $book->cover,
-				'review' => $book->bookreview
+				'rating'      => $book->rating,
+				'cover'       => $book->cover,
+				'review'      => $book->bookreview,
 			);
 		}
 
@@ -151,19 +151,18 @@ class Book_Sync_Cli extends WP_CLI_Command {
 
 	public function add_book( $book_details ) {
 		WP_CLI::line( 'Importing ' . $book_details['title'] );
-		//@todo Make sure the book doesn't already exist - if it does, update instead of insert
-		$post = array(
+		// @todo Make sure the book doesn't already exist - if it does, update instead of insert
+		$post     = array(
 			'post_content' => $book_details['review'],
-			'post_title' => $book_details['title'],
-			'post_type' => 'book',
+			'post_title'   => $book_details['title'],
+			'post_type'    => 'book',
 		);
 		$new_book = wp_insert_post( $post, true );
-		if( is_wp_error( $new_book ) ) {
+		if ( is_wp_error( $new_book ) ) {
 			WP_CLI::error( print_r( $new_book ) );
 		} else {
 			WP_CLI::line( 'Created book with post id ' . $new_book );
 		}
-
 
 	}
 
--- admin/class-book-sync-options.php
+++ admin/class-book-sync-options.php
@@ -51,14 +51,14 @@ class Book_Sync_Options {
 	 * Initialize the class and set its properties.
 	 *
 	 * @since      1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
+	 * @param      string $plugin_name       The name of this plugin.
+	 * @param      string $version    The version of this plugin.
 	 */
 	public function __construct( $plugin_name, $version ) {
 
 		$this->plugin_name = $plugin_name;
-		$this->version = $version;
-		$this->options = $this->get_options();
+		$this->version     = $version;
+		$this->options     = $this->get_options();
 
 	}
 
@@ -71,9 +71,9 @@ class Book_Sync_Options {
 	public function get_options() {
 		$defaults = array(
 			'librarything_username' => '',
-			'librarything_user_key' => ''
+			'librarything_user_key' => '',
 		);
-		$options = wp_parse_args( get_option( 'book_sync_settings' ), $defaults );
+		$options  = wp_parse_args( get_option( 'book_sync_settings' ), $defaults );
 		return $options;
 	}
 
@@ -115,7 +115,7 @@ class Book_Sync_Options {
 			'book_sync_settings',
 			'book_sync_settings_section',
 			array(
-				esc_attr__( 'Enter your LibraryThing username', 'book-sync' )
+				esc_attr__( 'Enter your LibraryThing username', 'book-sync' ),
 			)
 		);
 
@@ -127,7 +127,7 @@ class Book_Sync_Options {
 			'book_sync_settings_section',
 			array(
 				// translators: URL for finding user key on LibraryThing.com
-				sprintf( __( 'To find your LibraryThing user key, go to <a href="%s" target="_blank">LibraryThing</a>.  In the code on that page, you will see "key=XXXXXXXXXX": that is your user key.', 'book-sync' ), 'http://www.librarything.com/api/json.php' )
+				sprintf( __( 'To find your LibraryThing user key, go to <a href="%s" target="_blank">LibraryThing</a>.  In the code on that page, you will see "key=XXXXXXXXXX": that is your user key.', 'book-sync' ), 'http://www.librarything.com/api/json.php' ),
 			)
 		);
 
@@ -185,7 +185,7 @@ class Book_Sync_Options {
 	 *
 	 * @since 1.0.0
 	 */
-	public function settings_section_callback(  ) {
+	public function settings_section_callback() {
 		esc_attr_e( 'Enter your LibraryThing and Goodreads credentials to sync your libraries.', 'book-sync' );
 	}
 
@@ -194,7 +194,7 @@ class Book_Sync_Options {
 	 *
 	 * @since 1.0.0
 	 */
-	public function options_page(  ) {
+	public function options_page() {
 		?>
 		<h2><?php esc_attr_e( 'Book Sync', 'book-sync' ); ?></h2>
 		<form action='options.php' method='post'>
