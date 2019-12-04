e27beda6257e94ca129ed97d7d1cde409496ac11
wpalchemist <morgan@wpalchemists.com>
2019-12-04 11:00:46 -0800

    write unit test to check if we can create books

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -13,8 +13,19 @@ class BookSyncTest extends WP_UnitTestCase {
 	/**
 	 * A single example test.
 	 */
-	public function test_sample() {
-		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+	public function text_book_cpt() {
+		$book = $this->factory->post->create( array( 'post_title' => 'Test Book', 'post_type' => 'book' ) );
+
+		$query = new WP_Query( array(
+			's' => 'Test Book'
+		) );
+
+		$args = array(
+			'post_title' => 'Test Book'
+		);
+
+		$posts = $query->query( $args );
+
+		$this->assertEqualSets( array( $book ), wp_list_pluck( $posts, 'ID' ) );
 	}
 }
