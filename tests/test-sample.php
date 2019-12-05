107539b392f211967183548a0facf9744bc526fc
wpalchemist <morgan@wpalchemists.com>
2019-12-05 10:49:29 -0800

    pass PHPCS

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -14,12 +14,19 @@ class BookSyncTest extends WP_UnitTestCase {
 	 * Test whether the Book CPT exists.
 	 */
 	public function test_book_cpt() {
-		$book = $this->factory->post->create( array( 'post_title' => 'Test Book', 'post_type' => 'book' ) );
+		$book = $this->factory->post->create(
+			array(
+				'post_title' => 'Test Book',
+				'post_type'  => 'book',
+			)
+		);
 		$test = false;
 
-		$query = new WP_Query( array(
-			's' => 'Test Book'
-		) );
+		$query = new WP_Query(
+			array(
+				's' => 'Test Book',
+			)
+		);
 
 		while ( $query->have_posts() ) {
 			$query->the_post();
