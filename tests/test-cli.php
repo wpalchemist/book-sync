b1795bf1c3a073d55239c21983459c1e31b5c9d5
wpalchemist <morgan@wpalchemists.com>
2020-01-02 13:56:31 -0800

    update git modules

--- tests/test-cli.php
+++ tests/test-cli.php
@@ -10,11 +10,21 @@
  */
 class BookSyncCliTest extends WP_UnitTestCase {
 
-
 	/**
 	 * Test the book sync command with LibraryThing.
 	 */
 	public function test_book_sync_librarything() {
+		$Book_Sync_Cli = new Book_Sync_Cli();
+		$args = '';
+		$assoc_args = array(
+			'number' => 10,
+			'source' => 'LibraryThing',
+		);
+
+		$Book_Sync_Cli->import_books( $args, $assoc_args );
+
+
+
 
 	}
 
